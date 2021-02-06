fc51bbd8b63a49e436addfd130aacad6439034f4
alfonso-miguel-uia <alfonso.miguel.reyes.777@gmail.com>
2021-02-05 18:08:55 -0600

    Commit inicial AS

---
+++ contabilidadUIA/src/main/java/uia/com/contabilidad/Principal.java
@@ -0,0 +1,25 @@
+package uia.com.contabilidad;
+
+import uia.com.contabilidad.gestor.DecoradorCobranza;
+import uia.com.contabilidad.gestor.Gestor;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hola UIA");
+		
+		Gestor contabilidad = new Gestor("C:\\TSU-UIA\\contabilidadUIA\\cuentasXCobrar.json");
+		
+		 DecoradorCobranza gestorCobranza = new DecoradorCobranza(contabilidad);
+		 
+		 gestorCobranza.Print();
+		 gestorCobranza.validaCobranza();
+		//miGestorClientes.registraCheque("Alfonso", "Cheques", "ClienteX");
+		
+		
+	}
+
+
+}
